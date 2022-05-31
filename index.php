b7d7fdcc3f99c68f3134d3733d5bae209eed65bc
Breno Oliveira <breno6@ba.estudante.senai.br>
2022-05-31 12:07:30 -0300

    Header/Nav

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  
+    <title>Summit Dev</title>
+    <link rel="stylesheet" href="./css/style.css">
+   
+</head>
+<body>
+    <header class="header container">
+        <div class="container navbar">
+            <div class="logoNav"> 
+                <img class="flexCenter" src="./img/LogoNav.svg" alt="summitdevlogo">  
+            </div>
+            <nav>
+                <ul class=flexCenter >
+                    <li>Quem Somos</li>
+                    <li>Serviços</li>
+                    <li>Contato</li>
+                </ul>
+            </nav>
+            <nav class="flexCenter socialMidia">
+                <div>
+                    <img class="socialMidia" src="./img/navInsta.png" alt="">
+                    <img class="socialMidia" src="./img/NavZap.png" alt="">
+                    <img class="socialMidia" src="./img/Navfb.png" alt="">
+                </div>
+                
+            </nav>
+
+        </div>
+        <div class="mainNav">
+            <div id="mainbg">
+            
+            <h1>ALCANCE O PONTO MAIS ALTO .</h1>
+            <h2>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</h2>
+            </div>            
+                                   
+        </div>
+
+    </header>
+   
+    
+</body>
+</html>
